6cb003417b9c1ee73fb4f930b50f490b6234e8f1
earthlydev <jarizala.dev@gmail.com>
2022-11-10 22:45:44 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaHandPaper, FaHandRock, FaHandScissors } from 'react-icons/fa';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Let's Play</h1>
+      <div>
+        <div className="container">
+          <div className="player">
+            <div>You: 0</div>
+            <div></div>
+          </div>
+          <div className="player">
+            <div>Bot: 0</div>
+            <div></div>
+          </div>
+        </div>
+        <div>
+          <button className="btn">
+            <FaHandRock size={35}/>
+          </button>
+          <button className="btn">
+            <FaHandPaper size={35}/>
+          </button>
+          <button className="btn">
+            <FaHandScissors size={35}/>
+          </button>
+        </div>
+        <div className="container-2">
+          <h2>Player 1 Wins</h2>
+          <h3>Play Again?</h3>
+        </div>
+      </div>
     </div>
   );
 }
