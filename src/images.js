aca3e681f237de9e5e46b8d4728a0ae2a9098837
earthlydev <jarizala.dev@gmail.com>
2022-11-23 15:53:19 -0800

    added images

---
+++ src/images.js
@@ -0,0 +1,26 @@
+export const imgs = {
+    rock: {
+        title: 'rock',
+        src: require('.//assets/rock.png')
+    },
+    paper: {
+        title: 'paper',
+        src: require('.//assets/paper.png')
+    },
+    scissors: {
+        title: 'scissors',
+        src: require('.//assets/scissors.png')
+    },
+    botRock: {
+        title: 'bot rock',
+        src: require('.//assets/bot-rock.png')
+    },
+    botPaper: {
+        title: 'bot paper',
+        src: require('.//assets/paper.png')
+    },
+    botScissors: {
+        title: 'bot scissors',
+        src: require('.//assets/scissors.png')
+    }
+}
