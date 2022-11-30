891e7c3cd1e6e4d366391a322a845ddaa2ab765e
earthlydev <jarizala.dev@gmail.com>
2022-11-30 11:21:01 -0800

    adding player bot components

--- src/App.js
+++ src/App.js
@@ -2,31 +2,18 @@ import './App.css';
 import { FaHandPaper, FaHandRock, FaHandScissors } from 'react-icons/fa';
 import { imgs } from './images';
 
-function random() {
-  
-}
 
-function Player() {
+function Player({name = "", score = 0}) {
   return ( 
     <div className="player">
-      <div>You: 0</div>
-      <div>
+      <div className="score">{`${name}: ${score}`}</div>
+      <div className="action">
         <img src={ imgs.rock.src } alt={ imgs.paper.title } />
         </div>
     </div>  
   )
 }
 
-function Bot() {
-  return (
-    <div className="player">
-      <div>Bot: 0</div>
-      <div>
-          <img src={ imgs.botPaper.src } alt={ imgs.botPaper.title } />
-      </div>
-    </div>
-  )   
-}
 
 function App() {
   return (
@@ -34,8 +21,8 @@ function App() {
       <h1>Let's Play</h1>
       <div>
         <div className="container">
-          <Player /> 
-          <Bot />
+          <Player name="Player" /> 
+          <Player name="Bot" />
         </div>
         <div>
           <button className="btn">
